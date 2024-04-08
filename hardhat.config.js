e512d56a390813361b36edc7b8aa7ccc878b05a5
K2302 <95227135+K2302@users.noreply.github.com>
2024-04-09 00:05:37 +0530

    contract edited succesfully

--- hardhat.config.js
+++ hardhat.config.js
@@ -14,25 +14,25 @@
 // require("dotenv").config();
 // /** @type import('hardhat/config').HardhatUserConfig */
 
-// const SEPOLIA_URL = process.env.SEPOLIA_URL;
-// const PRIVATE_KEY = process.env.PRIVATE_KEY;
-// module.exports = {
-//   solidity: "0.8.17",
-//   networks: {
-//     sepolia: {
-//       url: SEPOLIA_URL,
-//       accounts: [PRIVATE_KEY],
-//     },
-//   },
-// };
-require("@nomicfoundation/hardhat-toolbox");
-
-/** @type import('hardhat/config').HardhatUserConfig */
+const SEPOLIA_URL = process.env.SEPOLIA_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 module.exports = {
-  solidity: "0.8.18",
+  solidity: "0.8.17",
   networks: {
-    hardhat: {
-      chainId: 31337,
+    sepolia: {
+      url: SEPOLIA_URL,
+      accounts: [PRIVATE_KEY],
     },
   },
 };
+// require("@nomicfoundation/hardhat-toolbox");
+
+// /** @type import('hardhat/config').HardhatUserConfig */
+// module.exports = {
+//   solidity: "0.8.18",
+//   networks: {
+//     hardhat: {
+//       chainId: 31337,
+//     },
+//   },
+// };
